107e26a0cf1e3a44507eaf833c7e7aec75da5cec
Louistwee <louistwee@skynet.be>
2017-10-21 13:38:21 +0200

    Update paint.js

--- paint.js
+++ paint.js
@@ -1,3 +1,19 @@
 $(function(){
-  alert('hoi');
+  var canvas = $('#canvas');
+  var buttonbar = $('#actionButtons')
+  var ctx = canvas[0].getContext('2d');
+  var color = '#ff00ff';
+  var actions = {
+    line:{
+      icon:'line.png',
+      action:function(){
+        ctx.moveTo(0,0);
+        ctx.lineTo(200,100);
+        ctx.stroke();
+      }
+    }
+  }
+  for(var i in actions){
+    buttonbar.append($('<button></button>').text('i').on('click',actions[i].action))
+  }
 })
