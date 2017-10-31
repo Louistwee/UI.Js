41ffedc16d6c84e2c269a8c2721b8129d249c091
Louistwee <louistwee@skynet.be>
2017-10-31 12:09:15 +0100

    Update UI.js

--- UI.js
+++ UI.js
@@ -1,123 +1,49 @@
-var logindex = 0;
-function l(a){
-  console.log((logindex++) + a);
-}
 //Create the base object
 window.UI = (function(window){
   var UI = {
-    //get the basePath of UI.js 
-    basePath:(function(window){
-      var scripts = document.getElementsByTagName('script');
-      for (var i = 0; i < scripts.length; i++) {
-        if(scripts[i].src.split('UI.js').length > 1){
-          var basePath = scripts[i].src.split('UI.js')[0];
+    //get the base URL of UI.js
+    baseURL:(function(window){
+      var i;
+      var basePath;
+      var scripts = document.getElementsByTagName("script");
+      for (i = 0; i < scripts.length; i+=1) {
+        if(scripts[i].src.split("UI.js").length > 1){
+          basePath = scripts[i].src.split("UI.js")[0];
         }
       }
       return basePath;
     })(window),
-    //URL: string relative to 
-    getURL:function(URL){
-      return this.basePath + URL;
+    /**
+    * This function puts UI.baseURL and @param {string} path togheter.
+    * @param {string} path A path relative to UI.js
+    * @return {string} An url.
+    */
+    getFullURL:function(path){
+      return this.baseURL + path;
     },
-    addSubObject:function(param){
-      this[param.name] = param.mainFn;
-      var sub = this[param.name];
-      for(var i in param.properties){
-        sub[i] = param.properties[i];
-      }
-      sub.__proto__ = this;
-      sub.parent = this;
-      sub.name = param.name;
-      return sub;
-    },
-    //list of all functions that are appended when an object is created;
-      //get/load an object from the UI 
-      get:function(objectName){
-        if(this.paused){
-          this.pauseList.push({fn:this.get,param:objectName});
-          return this;
-        }
-        if(this[objectName]){
-          return this[objectName];
-          //return this;
-        }else{
-          function callback(th){
-            th.start();
-          }
-          this.loadScript({
-            objectName:objectName,
-            callback:callback,
-          });
-          return this.pause();
-        }
-      },
-      //use .run() if you want ot add the fn to the pauseList otherwise when you want to run it directly
-      run:function(fnName,param){
-        var th = this;
-        var pauseFn = function(){
-          if(!th[fnName]){
-            function callback(th){
-              th.start();
-            }
-            th.loadScript({
-              objectName:fnName,
-              callback:callback,
-            });
-            th.pause();
-            th.pauseList.splice(0,0,{fn:pauseFn,param:param})
-          }else{
-            return th[fnName](param);
-          }
-        }
-        if(this.paused){
-          this.pauseList.push({fn:pauseFn,param:param});
-        }else{
-          pauseFn();
-        }
-        return this;
-      },
-      pause:function(){
-        this.paused = true;
-        this.pauseList = [];
-        return this;
-      },
-      start:function(){
-        this.paused = false;
-        while (!(!this.pauseList.length||this.paused)) {
-          var method = this.pauseList.splice(0, 1)[0];
-          var answ = method.fn.call(this,method.param);
-        }
-        return this;
-      },
-      name:'UI',
-    getPath:function(){
-      if(this.parent){
-        this.parent.getPath() + '/' + this.name;
-      }else{
-        return this.name;
-      }
-    },
-    getFullPath:function(param){
-      return this.basePath + '/' + this.getPath() + '/' + param.objectName + '.js';
-    },
-      //this is a test fn
-      loadScript:function(param){
-        var th = this;
-        var script = document.createElement('script');
-        var anotherscript = document.getElementsByTagName('script')[0];
-        var once = false;
-        script.type = 'text/javascript';
-        script.src = this.getFullPath(param);
+    scriptLoader:{
+      /**
+      * This functions loads a script.
+      * @param {string} url An URL pointing to the script.
+      * @param {function} allback A function called when the script is loaded.
+      * @return {boolean} True when the script is loaded already.
+      */
+      load:function(url,callback){
+        var script = document.createElement("script");
+        var anotherscript = document.getElementsByTagName("script")[0];
+        script.setAttribute("type", "text/javascript");
+        script.setAttribute("src", url);
         script.onload = script.onreadystatechange = function(){
-          if ( !once && (!this.readyState || this.readyState == 'complete') ){
-            once = true;
-            if(param.callback){
-              param.callback(th);
-            }
+          if (!this.readyState || this.readyState == "complete"){
+            setTimeout( function() {
+              callback( true );
+            }, 0 );
           }
-        }
+        };
         anotherscript.parentNode.insertBefore(script, anotherscript);
+        return ;
       },
-  }
+    },
+  };
   return UI;
 })(window);
