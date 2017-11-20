35611c0a036d1c37cf62adb6224f7f443657afa7
Louistwee <louistwee@skynet.be>
2017-11-20 18:13:24 +0100

    Update dom.js

--- UI/dom.js
+++ UI/dom.js
@@ -5,6 +5,9 @@ UI.dom = function(object){
     this.$ = object;
   }
 }
+UI.dom.isElement = function(element){
+  return typeof HTMLElement === "object" ? element instanceof HTMLElement : element && typeof element === "object" && element !== null && element.nodeType === 1 && typeof element.nodeName==="string"
+}
 // var button = new UI.dom(document.createElement('button')) 
 // button.get('equals').run(button).fn(function(a){if(a){alert('they are the same')}})
 UI.dom.prototype.__proto__ = UI.subClass.prototype;
