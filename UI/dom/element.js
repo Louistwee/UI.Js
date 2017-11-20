208bcf89528c56e0079d45357363459a9735f4df
Louistwee <louistwee@skynet.be>
2017-11-20 16:45:10 +0100

    Create element.js

---
+++ UI/dom/element.js
@@ -0,0 +1,13 @@
+UI.dom.element = function(obj){
+  if(typeof obj === 'string){
+  
+  }
+  if(typeoobj)
+  obj.$ = document.createElement(obj.type);
+  if(obj.parent){
+    obj.parent.get('append').run(el)
+  }
+  return el;
+};
+
+UI.dom.element.prototype.__proto__ = UI.dom.prototype;
