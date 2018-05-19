acdcf6378df0cbd2228facbc3c066dd29891a284
Louistwee <27563848+Louistwee@users.noreply.github.com>
2018-05-19 15:19:47 +0200

    Update color.js

--- UI/color.js
+++ UI/color.js
@@ -8,7 +8,11 @@ UI.color = function(name){
 };
 UI.color.colors = {
   'carrot':[230, 126, 34,1], // all colors in [R,G,B,A]
+  'pumpkin':[211, 84, 0,1],
+  'turqoise':[26, 188, 156,1],
+  'grean_sea':[22, 160, 133,1],
 };
+
 UI.color.prototype.RGBA = function(){
   var arr = this.color;
   return 'rgba('+arr[0]+','+arr[1]+','+ arr[2]+','+arr[3]+')';
